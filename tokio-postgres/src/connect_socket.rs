6fb34ded3214af028aae8d65725726f4aaa7f77c
Nikolay Kim <fafhrd91@gmail.com>
2022-01-03 23:15:31 +0600

    remove tokio dep

--- tokio-postgres/src/connect_socket.rs
+++ tokio-postgres/src/connect_socket.rs
@@ -1,10 +1,6 @@
 use std::{future::Future, io, time::Duration};
 
-use ntex::{io::Io, util::PoolId};
-use tokio::net::TcpStream;
-#[cfg(unix)]
-use tokio::net::UnixStream;
-use tokio::time;
+use ntex::{rt, time, io::Io, util::PoolId, connect};
 
 use crate::config::Host;
 use crate::{Error, Socket};
@@ -21,17 +17,19 @@ pub(crate) async fn connect_socket(
 
     match host {
         Host::Tcp(host) => {
+            let fut = connect::connect(connect::Connect::new(host.clone()).set_port(port));
             let socket =
-                connect_with_timeout(TcpStream::connect((&**host, port)), connect_timeout).await?;
-            socket.set_nodelay(true).map_err(Error::connect)?;
-
-            Ok(Io::with_memory_pool(socket, PoolId::P10.pool_ref()))
+                connect_with_timeout(
+                    async move {
+                        fut.await.map_err(|e| io::Error::new(io::ErrorKind::Other, "connect error"))
+                    },
+                    connect_timeout).await?;
+            socket.set_memory_pool(PoolId::P10.pool_ref());
+            Ok(socket)
         }
         #[cfg(unix)]
         Host::Unix(path) => {
-            let path = path.join(format!(".s.PGSQL.{}", port));
-            let socket = connect_with_timeout(UnixStream::connect(path), connect_timeout).await?;
-            Ok(Io::with_memory_pool(socket, PoolId::P10.pool_ref()))
+            panic!("not supported")
         }
     }
 }
